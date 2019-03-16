3962b22e4992ae85ef0e21c36c7106926ea289ae
lenovo <lenovo@surya>
2019-03-17 01:24:32 +0530

    concepts

---
+++ Conceptspractice/src/Hashcode.java
@@ -0,0 +1,8 @@
+
+public class Hashcode {
+public static void main(String[] args) {
+	Hashcode hc=new Hashcode();
+	System.out.println(hc);//object address
+	System.out.println(hc.hashCode());//integer representation of object adderess
+}
+}
