44cac0c528cf4e401becf16730b0f934f1a369ba
lenovo <lenovo@surya>
2019-03-17 01:54:54 +0530

    test

---
+++ Test/src/Test.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Test 
+{
+	public static void main(String args[])
+	{
+		System.out.println("main method started");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Number");
+		int a=sc.nextInt();
+		System.out.println("square of 'a' is "+a*a);
+		System.out.println("Thank you");
+	}
+	}
