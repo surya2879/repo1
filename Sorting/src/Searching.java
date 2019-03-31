f129c29153bf5fc5203945f5e452813cf47ef2c0
lenovo <lenovo@surya>
2019-04-01 00:02:42 +0530

    linear

--- Sorting/src/Searching.java
+++ Sorting/src/Searching.java
@@ -4,7 +4,7 @@ public class Searching {
 	  public static void main(String[] args) {
 		  Scanner sc=new Scanner(System.in);    //linear search
 		  int i;
-		  int arr[]= {10,20,30,40,50,60,};
+		  int arr[]= {10,20,30,40,50,60};
 	      System.out.println("enter element to search");
 		  int ele=sc.nextInt();
 		  for(i=0;i<arr.length;i++) 
