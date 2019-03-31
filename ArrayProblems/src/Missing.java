1151ee4cfbc011f2af369ecd2300a0d722ea79b3
lenovo <lenovo@surya>
2019-03-31 21:54:30 +0530

    missing

--- ArrayProblems/src/Missing.java
+++ ArrayProblems/src/Missing.java
@@ -1,14 +1,17 @@
-
-public class Missing {
-public static void main(String[] args) {
-	int[] arr= {1,2,3,5};
-	int sum=0;
-	int n=arr.length+1;
-	for(int i=0;i<arr.length;i++)
-	{
-		sum=sum+arr[i];
+public class Missing 
+{
+	public static void main(String[] args) {
+		   int[] arr= {1,2,3,5};
+			int sum=0;
+			int n=arr.length+1;
+			for(int i=0;i<arr.length;i++)
+			{
+				sum=sum+arr[i];
+			}
+			int total=(n*(n+1))/2;
+			System.out.println("missing term is : " + (total-sum));
 	}
-	int total=(n*(n+1))/2;
-	System.out.println("missing term is : " + (total-sum));
+ 
  }
-}
+
+
---
+++ ArrayProblems/src/MissingNumbers.java
@@ -0,0 +1,19 @@
+public class MissingNumbers {
+   
+	static void findMissingNumber(int[] arr) {
+        int currentValue =1;
+        int max=10;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != currentValue) {
+                for (int j = currentValue; j < max; j++) {
+                    System.out.println("Missing number Found : " + j);
+                }
+        }
+            currentValue = arr[i] + 1;
+       }
+    }
+    public static void main(String[] args) {
+      int[] arr = {1,2,10};
+      findMissingNumber(arr);
+   }
+}
