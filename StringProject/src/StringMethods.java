d63bb0fa6a41aa6e6d7a8f9edad9d7afada731b3
lenovo <lenovo@surya>
2019-04-03 07:17:01 +0530

    string methods

---
+++ StringProject/src/StringMethods.java
@@ -0,0 +1,12 @@
+
+public class StringMethods {
+public static void main(String[] args) {
+	String str="suryaprakash";
+	System.out.println(str.charAt(4));
+	System.out.println(str.indexOf('p'));
+	System.out.println(str.toUpperCase());
+	System.out.println(str.toLowerCase());
+	System.out.println(str.length());
+	System.out.println(str.substring(5,12));
+ }
+}
