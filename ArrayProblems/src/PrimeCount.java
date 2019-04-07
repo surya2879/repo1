6b8f5c0cc719062bc02c471e05c9556cebad38ec
lenovo <lenovo@surya>
2019-04-07 12:11:33 +0530

    prime count

---
+++ ArrayProblems/src/PrimeCount.java
@@ -0,0 +1,26 @@
+
+public class PrimeCount {
+	public static boolean isPrime(int num)
+	{
+		for(int i=2;i<=num-1;i++)               //prime
+		{
+			if(num%i==0)
+			{
+				return false;
+			}
+		}
+		return true;
+}
+ public static void main(String[] args) { 
+ int [] arr= {4,5,6,7,11,15,16};
+ int count=0;
+ for(int i=0;i<arr.length;i++)
+ {
+	if(isPrime(arr[i]))
+	{
+		count++;
+	}
+ }
+ System.out.println("no. of prime elements in array are:"+count);
+}
+}
